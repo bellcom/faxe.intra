b644d2b1168639704c0574836c5f0375d03b4266
juuliabellcom <juulia@bellcom.dk>
2017-08-16 17:40:01 +0300

    FAXE-62 4th column on user import settings page

--- public_html/profiles/os2intra/modules/os2intra/custom/os2intra_user_import/js/os2intra_user_import.js
+++ public_html/profiles/os2intra/modules/os2intra/custom/os2intra_user_import/js/os2intra_user_import.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,20 +8,24 @@
     attach: function (context, settings) {
       $('.os2intra-mapping-fallback-field').each(function( index ){
          if ($(this).val() == 'input') {
-          $(this).parent('.form-item').next('.form-item').children('input[type="text"]').show();
+          $(this).parent('.form-item').next('.form-item').children('input[type="text"]').css('visibility', 'visible');
         }
         else {
-          $(this).parent('.form-item').next('.form-item').children('input[type="text"]').hide();
+          $(this).parent('.form-item').next('.form-item').children('input[type="text"]').css('visibility', 'hidden');
         }
       });
       $('.os2intra-mapping-fallback-field').change(function (event) {
         if ($(this).val() == 'input') {
-          $(this).parent('.form-item').next('.form-item').children('input[type="text"]').show();
+          $(this).parent('.form-item').next('.form-item').children('input[type="text"]').css('visibility', 'visible');
         }
         else {
-          $(this).parent('.form-item').next('.form-item').children('input[type="text"]').hide();
+          $(this).parent('.form-item').next('.form-item').children('input[type="text"]').css('visibility', 'hidden');
         }
       });
+       $('.os2intra-mapping-primary-field').change(function (event) {
+          $('.os2intra-mapping-markup-field').hide();
+      });
+
     }
   }
 })(jQuery);
